ed66f5a05e3119f2d272f022db125beb8d1f5505
Sachin <mittalsam98@gmail.com>
2022-07-26 17:27:55 +0530

    added url

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,13 +1,13 @@
 import { io } from "socket.io-client";
 
-const URL='http://localhost:5000/';
+const URL='https://chess-web-online.herokuapp.com/';
 
 const socket=io(URL);
 var mySocketId
 console.log('dfas');
 
 socket.on("createNewGame", statusUpdate => {
-    console.log("A new gamye has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
+    console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
     mySocketId = statusUpdate.mySocketId
 })
 
