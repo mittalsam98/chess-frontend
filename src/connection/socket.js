dfd7dd7a21cbc0deb096e447170ee9f84366e030
Sachin <mittalsam98@gmail.com>
2023-03-29 12:01:52 +0530

    Changed Heroku Endpoint to Vercel

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://chess-web-online.herokuapp.com/';
+// const URL = 'https://chess-web-online.herokuapp.com/';
+const URL = 'https://chess-web-online.vercel.app/';
 
 const socket = io(URL);
 var mySocketId;
